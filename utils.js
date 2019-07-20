4e4d4a058b96e004558ae3d9be6f10aadc77d8f8
ebundala <ebundala@gmail.com>
2019-07-20 03:31:20 +0300

    replace special html chars

--- utils.js
+++ utils.js
@@ -8,9 +8,10 @@ module.exports={
     .replace(new RegExp("&lt;","g")," ")
     .replace(new RegExp("&gt;","g")," ")
     .replace(new RegExp("&nbsp;","g")," ")
-    .replace(new RegExp("&iexcl","g")," ")
+    .replace(new RegExp("&iexcl;","g")," ")
     .replace(new RegExp("&laquo;","g")," ")
-    .replace(new RegExp("&raquo","g")," ")
+    .replace(new RegExp("&raquo;","g")," ")
+    .replace(new RegExp("&#039;","g"),"")
 }
 
 }
