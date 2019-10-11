6901a23f9bf84b0ea77e6c08e65bbef6ca37175e
Elias Bundala <ebundala@gmail.com>
2019-10-11 04:31:06 +0300

    notification query enhancement

--- server_v2/server/models/Notifications.js
+++ server_v2/server/models/Notifications.js
@@ -10,7 +10,7 @@ export default class Notifications extends ArangoDatasource {
   getNotifications({ _id }, { offset, limit }) {
     if (!_id) throw new Error('User is not logedin');
     const q = aql`
-    FOR action, e, p IN 1..3 ANY ${_id} Follows,Comment,\`Like\`,Reported
+    FOR subject, e, p IN 1..2 ANY ${_id} Follows,Comment,\`Like\`,Reported
     OPTIONS {
     bfs:true,
     uniqueVertices: 'global',
@@ -18,7 +18,7 @@ export default class Notifications extends ArangoDatasource {
     }
     SORT e.createdAt DESC
     LIMIT ${offset},${limit}
-    RETURN {action:e, subject:(RETURN DOCUMENT(e._to))[0] , actor:(RETURN DOCUMENT(e._from))[0]}
+    RETURN {action:e, subject , actor:(RETURN DOCUMENT(e._from))[0]}
      `;
     return this.db.query(q).then((arr) => arr.all());
   }
